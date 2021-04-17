e557395698c45f2883cdab2fe0d9515eb3d866c7
kkoyan <71924575+kkoyan@users.noreply.github.com>
2021-04-17 23:54:10 +0900

    条件分岐

---
+++ Lesson10_01.java
@@ -0,0 +1,9 @@
+class Lesson10_01{//条件分岐
+    public static void main(String[] args){
+        int age = 22;
+        
+        if (age >=20){
+            System.out.println("adult");
+        }
+    }
+}
---
+++ Lesson10_03.java
@@ -0,0 +1,11 @@
+class Lesson10_03{//条件分岐
+    public static void main(String[] args){
+        int age = 22;
+        
+        if (age >=20){//条件A
+            System.out.println("adult");//条件Aを満たした時の処理
+        } else {
+            System.out.println("child");//条件Aを満たさなかった時の処理
+        }
+    }
+}
